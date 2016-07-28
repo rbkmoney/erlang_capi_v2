bc2924f190e9c27097e3c4c033b43945a64776ee
Artem Ocheredko <galaxie.stern@gmail.com>
2016-07-28 18:43:33 +0300

    CAPI-25 Add new building with Jenkins and add real backend handler

---
+++ apps/capi/src/capi_woody_event_handler.erl
@@ -0,0 +1,22 @@
+-module(capi_woody_event_handler).
+-behaviour(woody_event_handler).
+
+-export([handle_event/3]).
+
+%%
+
+-spec handle_event(EventType, RpcID, EventMeta)
+    -> _ when
+        EventType :: woody_event_handler:event_type(),
+        RpcID ::  woody_t:rpc_id(),
+        EventMeta :: woody_event_handler:event_meta_type().
+
+handle_event(EventType, RpcID, #{status := error, class := Class, reason := Reason, stack := Stack}) ->
+    lager:error(
+        maps:to_list(RpcID),
+        "[server] ~s with ~s:~p at ~s",
+        [EventType, Class, Reason, genlib_format:format_stacktrace(Stack, [newlines])]
+    );
+
+handle_event(EventType, RpcID, EventMeta) ->
+    lager:debug(maps:to_list(RpcID), "[server] ~s: ~p", [EventType, EventMeta]).
