1597a556e4c89447bed35b0b0e33eacf10552dc1
Anton Belyaev <xantbel@gmail.com>
2017-05-10 21:35:17 +0300

    CAPI-137: introduce capi access log

--- apps/capi/src/capi_swagger_server.erl
+++ apps/capi/src/capi_swagger_server.erl
@@ -1,12 +1,15 @@
 -module(capi_swagger_server).
 
 -export([child_spec/1]).
+-export([request_hook/1]).
 -export([response_hook/4]).
 
 -define(APP, capi).
 -define(DEFAULT_ACCEPTORS_POOLSIZE, 100).
 -define(DEFAULT_IP_ADDR, "::").
 -define(DEFAULT_PORT, 8080).
+
+-define(START_TIME_TAG, processing_start_time).
 
 -type params() :: module().
 
@@ -36,21 +39,31 @@ get_cowboy_config(LogicHandler) ->
             cowboy_cors,
             cowboy_handler
         ]},
+        {onrequest, fun ?MODULE:request_hook/1},
         {onresponse, fun ?MODULE:response_hook/4}
     ].
+
+
+-spec request_hook(cowboy_req:req()) ->
+    cowboy_req:req().
+
+request_hook(Req) ->
+    cowboy_req:set_meta(?START_TIME_TAG, genlib_time:ticks(), Req).
 
 -spec response_hook(cowboy:http_status(), cowboy:http_headers(), iodata(), cowboy_req:req()) ->
     cowboy_req:req().
 
 response_hook(Code, Headers, _, Req) when Code >= 500 ->
     send_oops_resp(Code, Headers, get_oops_body_safe(Code), Req);
-response_hook(_, _, _, Req) ->
+response_hook(Code, Headers, _, Req) ->
+    _ = log_access(Code, Headers, Req),
     Req.
 
 %% cowboy_req:reply/4 has a faulty spec in case of response body fun.
 -dialyzer({[no_contracts, no_fail_call], send_oops_resp/4}).
 
-send_oops_resp(_, _, undefined, Req) ->
+send_oops_resp(Code, Headers, undefined, Req) ->
+    _ = log_access(Code,  Headers, Req),
     Req;
 send_oops_resp(Code, Headers, File, Req) ->
     FileSize = filelib:file_size(File),
@@ -71,6 +84,7 @@ send_oops_resp(Code, Headers, File, Req) ->
             {<<"content-length">>, integer_to_list(FileSize)}
         ]
     ),
+    _ = log_access(Code, Headers1, Req),
     {ok, Req1} = cowboy_req:reply(Code, Headers1, {FileSize, F}, Req),
     Req1.
 
@@ -84,3 +98,48 @@ get_oops_body_safe(Code) ->
 
 get_oops_body(Code) ->
     genlib_map:get(Code, genlib_app:env(?APP, oops_bodies, #{}), undefined).
+
+log_access(Code, Headers, Req) ->
+    {Method, _} = cowboy_req:method(Req),
+    {Path,   _} = cowboy_req:path(Req),
+    {ReqLen, _} = cowboy_req:body_length(Req),
+    {ReqId,  _} = cowboy_req:header(<<"x-request-id">>, Req, undefined),
+    Duration = get_request_duration(Req),
+    RemAddr  = get_remote_addr(Req),
+    RespLen  = get_response_len(Headers),
+    MD = [
+        {remote_addr,     RemAddr},
+        {request_method,  Method},
+        {request_path,    Path},
+        {request_length,  ReqLen},
+        {response_length, RespLen},
+        {request_time,    Duration},
+        {'http_x-request-id', ReqId},
+        {status, Code}
+    ],
+    _ = lager:md(orddict:merge(fun(_Key, New, _Old) -> New end, MD, lager:md())),
+    capi_access_logger:info("Request processed").
+
+get_remote_addr(Req) ->
+    case swagger_handler_api:determine_peer(Req) of
+        {{ok, #{ip_address := Ip}}, _} ->
+            genlib:to_binary(inet:ntoa(Ip));
+        {_, _} ->
+            undefined
+    end.
+
+get_request_duration(Req) ->
+    case cowboy_req:meta(?START_TIME_TAG, Req) of
+        {undefined, _} ->
+            undefined;
+        {StartTime, _} ->
+            (genlib_time:ticks() - StartTime) / 1000000
+    end.
+
+get_response_len(Headers) ->
+    case lists:keyfind(<<"content-length">>, 1, Headers) of
+        {_, Len} ->
+            genlib:to_int(Len);
+        false ->
+            undefined
+    end.
