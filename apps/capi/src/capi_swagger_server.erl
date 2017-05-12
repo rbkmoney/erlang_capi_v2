0d98eca7d998e04d12dc9196d83820dc67db888b
Anton Belyaev <arentrue@users.noreply.github.com>
2017-05-12 16:28:38 +0400

    CAPI-137: introduce capi access log (#51)

--- apps/capi/src/capi_swagger_server.erl
+++ apps/capi/src/capi_swagger_server.erl
@@ -1,12 +1,15 @@
 -module(capi_swagger_server).
 
 -export([child_spec/1]).
+-export([request_hook/1]).
 -export([response_hook/4]).
 
 -define(APP, capi).
 -define(DEFAULT_ACCEPTORS_POOLSIZE, 100).
 -define(DEFAULT_IP_ADDR, "::").
 -define(DEFAULT_PORT, 8080).
+
+-define(START_TIME_TAG, processing_start_time).
 
 -type params() :: module().
 
@@ -36,22 +39,44 @@ get_cowboy_config(LogicHandler) ->
             cowboy_cors,
             cowboy_handler
         ]},
+        {onrequest, fun ?MODULE:request_hook/1},
         {onresponse, fun ?MODULE:response_hook/4}
     ].
+
+-spec request_hook(cowboy_req:req()) ->
+    cowboy_req:req().
+
+request_hook(Req) ->
+    cowboy_req:set_meta(?START_TIME_TAG, genlib_time:ticks(), Req).
 
 -spec response_hook(cowboy:http_status(), cowboy:http_headers(), iodata(), cowboy_req:req()) ->
     cowboy_req:req().
 
-response_hook(Code, Headers, _, Req) when Code >= 500 ->
+response_hook(Code, Headers, _, Req) ->
+    try
+        {Code1, Headers1, Req1} = handle_response(Code, Headers, Req),
+        _ = log_access(Code1, Headers1, Req1),
+        Req1
+    catch
+        Class:Reason ->
+            Stack = genlib_format:format_stacktrace(erlang:get_stacktrace(), [newlines]),
+            _ = lager:warning(
+                "Response hook failed for: [~p, ~p, ~p]~nwith: ~p:~p~nstacktrace: ~ts",
+                [Code, Headers, Req, Class, Reason, Stack]
+            ),
+            Req
+    end.
+
+handle_response(Code, Headers, Req) when Code >= 500 ->
     send_oops_resp(Code, Headers, get_oops_body_safe(Code), Req);
-response_hook(_, _, _, Req) ->
-    Req.
+handle_response(Code, Headers, Req) ->
+    {Code, Headers, Req}.
 
 %% cowboy_req:reply/4 has a faulty spec in case of response body fun.
 -dialyzer({[no_contracts, no_fail_call], send_oops_resp/4}).
 
-send_oops_resp(_, _, undefined, Req) ->
-    Req;
+send_oops_resp(Code, Headers, undefined, Req) ->
+    {Code, Headers, Req};
 send_oops_resp(Code, Headers, File, Req) ->
     FileSize = filelib:file_size(File),
     F = fun(Socket, Transport) ->
@@ -72,7 +97,7 @@ send_oops_resp(Code, Headers, File, Req) ->
         ]
     ),
     {ok, Req1} = cowboy_req:reply(Code, Headers1, {FileSize, F}, Req),
-    Req1.
+    {Code, Headers1, Req1}.
 
 get_oops_body_safe(Code) ->
     try get_oops_body(Code)
@@ -84,3 +109,50 @@ get_oops_body_safe(Code) ->
 
 get_oops_body(Code) ->
     genlib_map:get(Code, genlib_app:env(?APP, oops_bodies, #{}), undefined).
+
+log_access(Code, Headers, Req) ->
+    {Method, _} = cowboy_req:method(Req),
+    {Path,   _} = cowboy_req:path(Req),
+    {ReqLen, _} = cowboy_req:body_length(Req),
+    {ReqId,  _} = cowboy_req:header(<<"x-request-id">>, Req, undefined),
+    RemAddr  = get_remote_addr(Req),
+    RespLen  = get_response_len(Headers),
+    Duration = get_request_duration(Req),
+    ReqMeta = [
+        {remote_addr, RemAddr},
+        {request_method, Method},
+        {request_path, Path},
+        {request_length, ReqLen},
+        {response_length, RespLen},
+        {request_time, Duration},
+        {'http_x-request-id', ReqId},
+        {status, Code}
+    ],
+    Meta = orddict:merge(fun(_Key, New, _Old) -> New end, ReqMeta, lager:md()),
+    %% Call lager:log/5 here directly in order to pass request metadata (fused into
+    %% lager metadata) without storing it in a process dict via lager:md/1.
+    lager:log(capi_access_lager_event, info, Meta, "", []).
+
+get_remote_addr(Req) ->
+    case swagger_handler_api:determine_peer(Req) of
+        {{ok, #{ip_address := IP}}, _} ->
+            genlib:to_binary(inet:ntoa(IP));
+        {_, _} ->
+            undefined
+    end.
+
+get_request_duration(Req) ->
+    case cowboy_req:meta(?START_TIME_TAG, Req) of
+        {undefined, _} ->
+            undefined;
+        {StartTime, _} ->
+            (genlib_time:ticks() - StartTime) / 1000000
+    end.
+
+get_response_len(Headers) ->
+    case lists:keyfind(<<"content-length">>, 1, Headers) of
+        {_, Len} ->
+            genlib:to_int(Len);
+        false ->
+            undefined
+    end.
