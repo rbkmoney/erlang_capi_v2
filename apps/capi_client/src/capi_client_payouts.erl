c47ac9863a5a442bf601de78359fd14ab389f7eb
Evgeny Levenets <levenets.e.e@gmail.com>
2017-09-08 14:08:30 +0300

    add test

--- apps/capi_client/src/capi_client_payouts.erl
+++ apps/capi_client/src/capi_client_payouts.erl
@@ -1,6 +1,7 @@
 -module(capi_client_payouts).
 
 -export([get_payout_tools/2]).
+-export([get_payout_tool_by_id/3]).
 
 -type context() :: capi_client_lib:context().
 
@@ -13,4 +14,16 @@ get_payout_tools(Context, ContractID) ->
     },
     {Url, PreparedParams, Opts} = capi_client_lib:make_request(Context, Params),
     Response = swag_client_payouts_api:get_payout_tools(Url, PreparedParams, Opts),
+    capi_client_lib:handle_response(Response).
+
+-spec get_payout_tool_by_id(context(), binary(), binary()) -> {ok, term()} | {error, term()}.
+get_payout_tool_by_id(Context, ContractID, PayoutToolID) ->
+    Params = #{
+        binding => #{
+            <<"contractID">> => ContractID,
+            <<"payoutToolID">> => PayoutToolID
+        }
+    },
+    {Url, PreparedParams, Opts} = capi_client_lib:make_request(Context, Params),
+    Response = swag_client_payouts_api:get_payout_tool_by_id(Url, PreparedParams, Opts),
     capi_client_lib:handle_response(Response).
