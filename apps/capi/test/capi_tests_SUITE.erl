fde7c045cbb1e9e31a06e333bba0ef76744f47d3
Evgeny Levenets <levenets.e.e@gmail.com>
2017-11-03 15:32:46 +0300

    fix old tests

--- apps/capi/test/capi_tests_SUITE.erl
+++ apps/capi/test/capi_tests_SUITE.erl
@@ -317,12 +317,15 @@ init_per_group(operations_by_invoice_template_access_token, Config) ->
     Req = #{
         <<"shopID">> => ?STRING,
         <<"lifetime">> => get_lifetime(),
-        <<"cost">> => #{
-            <<"invoiceTemplateCostType">> => <<"InvoiceTemplateCostFixed">>,
-            <<"currency">> => ?RUB,
-            <<"amount">> => ?INTEGER
+        <<"details">> => #{
+            <<"templateType">> => <<"InvoiceTemplateSingleLine">>,
+            <<"product">> => <<"test_invoice_template_product">>,
+            <<"price">> => #{
+                <<"costType">> => <<"InvoiceTemplateLineCostFixed">>,
+                <<"currency">> => ?RUB,
+                <<"amount">> => ?INTEGER
+            }
         },
-        <<"product">> => <<"test_invoice_template_product">>,
         <<"description">> => <<"test_invoice_template_description">>,
         <<"metadata">> => #{<<"invoice_template_dummy_metadata">> => <<"test_value">>}
     },
@@ -521,19 +524,22 @@ fulfill_invoice_ok_test(Config) ->
     _.
 create_invoice_template_ok_test(Config) ->
     mock_services([{invoice_templating, fun('Create', _) -> {ok, ?INVOICE_TPL} end}], Config),
-    Req = #{
+    Req0 = #{
         <<"shopID">> => ?STRING,
         <<"lifetime">> => get_lifetime(),
-        <<"cost">> => #{
-            <<"invoiceTemplateCostType">> => <<"InvoiceTemplateCostFixed">>,
-            <<"currency">> => ?RUB,
-            <<"amount">> => ?INTEGER
-        },
-        <<"product">> => <<"test_invoice_template_product">>,
         <<"description">> => <<"test_invoice_template_description">>,
         <<"metadata">> => #{<<"invoice_template_dummy_metadata">> => <<"test_value">>}
     },
-    {ok, _} = capi_client_invoice_templates:create(?config(context, Config), Req).
+    Details0 = #{
+        <<"templateType">> => <<"InvoiceTemplateSingleLine">>,
+        <<"product">> => <<"test_invoice_template_product">>,
+        <<"price">> => #{
+            <<"costType">> => <<"InvoiceTemplateLineCostFixed">>,
+            <<"currency">> => ?RUB,
+            <<"amount">> => ?INTEGER
+        }
+    },
+    {ok, _} = capi_client_invoice_templates:create(?config(context, Config), Req0#{<<"details">> => Details0}).
 
 -spec get_invoice_template_ok_test(config()) ->
     _.
@@ -546,14 +552,16 @@ get_invoice_template_ok_test(Config) ->
 update_invoice_template_ok_test(Config) ->
     mock_services([{invoice_templating, fun('Update', _) -> {ok, ?INVOICE_TPL} end}], Config),
     Req = #{
-        <<"cost">> => #{
-            <<"invoiceTemplateCostType">> => <<"InvoiceTemplateCostFixed">>,
-            <<"amount">> => ?INTEGER,
-            <<"currency">> => ?RUB
+        <<"details">> => #{
+            <<"templateType">> => <<"InvoiceTemplateSingleLine">>,
+            <<"product">> => <<"test_invoice_template_product">>,
+            <<"price">> => #{
+                <<"costType">> => <<"InvoiceTemplateLineCostFixed">>,
+                <<"currency">> => ?RUB,
+                <<"amount">> => ?INTEGER
+            }
         },
         <<"lifetime">> => get_lifetime(),
-        <<"product">> => <<"test_invoice_template_product">>,
-        <<"description">> => <<"test_invoice_template_description">>,
         <<"metadata">> => #{<<"invoice_template_dummy_metadata">> => <<"test_value">>}
     },
     {ok, _} = capi_client_invoice_templates:update(?config(context, Config), ?STRING, Req).
