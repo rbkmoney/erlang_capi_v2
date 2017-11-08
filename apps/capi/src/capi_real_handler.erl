ec5cb98e5b8aca48cb74a1eac42950b3705fa85a
Andrey Mayorov <a.mayorov@rbkmoney.com>
2017-11-08 19:09:35 +0300

    CDS-51: Allow for shorter cardnumber masks

--- apps/capi/src/capi_real_handler.erl
+++ apps/capi/src/capi_real_handler.erl
@@ -2506,7 +2506,7 @@ decode_bank_card_details(DetailsType, #domain_BankCard{
 
 -define(MASKED_PAN_MAX_LENGTH, 4).
 
-decode_masked_pan(MaskedPan) ->
+decode_masked_pan(MaskedPan) when byte_size(MaskedPan) >= ?MASKED_PAN_MAX_LENGTH ->
     binary:part(MaskedPan, {byte_size(MaskedPan), -?MASKED_PAN_MAX_LENGTH}).
 
 decode_contact_info(#domain_ContactInfo{
