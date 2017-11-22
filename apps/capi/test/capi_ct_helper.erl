3290f4b543a6ef04fc3da39d1430eabd29a8073f
Anton Belyaev <arentrue@users.noreply.github.com>
2017-11-22 21:01:00 +0300

    Bump swagger-codegen to 2.3.0 (#139)

--- apps/capi/test/capi_ct_helper.erl
+++ apps/capi/test/capi_ct_helper.erl
@@ -17,7 +17,7 @@ start_app(lager = AppName) ->
         {error_logger_hwm, 600},
         {suppress_application_start_stop, true},
         {handlers, [
-            {lager_common_test_backend, warning}
+            {lager_common_test_backend, [warning, {lager_logstash_formatter, []}]}
         ]}
     ]);
 
