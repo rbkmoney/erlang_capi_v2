e9194671c99d5836c3889b2cbf7e7453d9560f61
Andrew Mayorov <encube.ul@gmail.com>
2017-12-13 20:03:17 +0300

    CAPI-231: Put the notion of digital wallets into this shithole (#144)
    * CAPI-231: Proudly employ cowboy_access_log
    
    * CAPI-231: Switch to rbkmoney/damsel@9cb7c24 @ epic/qiwi
    
    * CAPI-231: Switch to rbkmoney/swag@c36fa39 @ epic/qiwi-backport
    
    * CAPI-231: Put the notion of digital wallets into this shithole

--- apps/capi/src/capi_swagger_server.erl
+++ apps/capi/src/capi_swagger_server.erl
@@ -39,7 +39,7 @@ get_cowboy_config(LogicHandler) ->
             cowboy_cors,
             cowboy_handler
         ]},
-        {onrequest, fun ?MODULE:request_hook/1},
+        {onrequest, cowboy_access_log:get_request_hook()},
         {onresponse, fun ?MODULE:response_hook/4}
     ].
 
@@ -52,10 +52,10 @@ request_hook(Req) ->
 -spec response_hook(cowboy:http_status(), cowboy:http_headers(), iodata(), cowboy_req:req()) ->
     cowboy_req:req().
 
-response_hook(Code, Headers, _, Req) ->
+response_hook(Code, Headers, Body, Req) ->
     try
         {Code1, Headers1, Req1} = handle_response(Code, Headers, Req),
-        _ = log_access(Code1, Headers1, Req1),
+        _ = log_access(Code1, Headers1, Body, Req1),
         Req1
     catch
         Class:Reason ->
@@ -110,49 +110,6 @@ get_oops_body_safe(Code) ->
 get_oops_body(Code) ->
     genlib_map:get(Code, genlib_app:env(?APP, oops_bodies, #{}), undefined).
 
-log_access(Code, Headers, Req) ->
-    {Method, _} = cowboy_req:method(Req),
-    {Path,   _} = cowboy_req:path(Req),
-    {ReqLen, _} = cowboy_req:body_length(Req),
-    {ReqId,  _} = cowboy_req:header(<<"x-request-id">>, Req, undefined),
-    RemAddr  = get_remote_addr(Req),
-    RespLen  = get_response_len(Headers),
-    Duration = get_request_duration(Req),
-    ReqMeta = [
-        {remote_addr, RemAddr},
-        {request_method, Method},
-        {request_path, Path},
-        {request_length, ReqLen},
-        {response_length, RespLen},
-        {request_time, Duration},
-        {'http_x-request-id', ReqId},
-        {status, Code}
-    ],
-    Meta = orddict:merge(fun(_Key, New, _Old) -> New end, ReqMeta, lager:md()),
-    %% Call lager:log/5 here directly in order to pass request metadata (fused into
-    %% lager metadata) without storing it in a process dict via lager:md/1.
-    lager:log(capi_access_lager_event, info, Meta, "", []).
-
-get_remote_addr(Req) ->
-    case swag_server_handler_api:determine_peer(Req) of
-        {{ok, #{ip_address := IP}}, _} ->
-            genlib:to_binary(inet:ntoa(IP));
-        {_, _} ->
-            undefined
-    end.
-
-get_request_duration(Req) ->
-    case cowboy_req:meta(?START_TIME_TAG, Req) of
-        {undefined, _} ->
-            undefined;
-        {StartTime, _} ->
-            (genlib_time:ticks() - StartTime) / 1000000
-    end.
-
-get_response_len(Headers) ->
-    case lists:keyfind(<<"content-length">>, 1, Headers) of
-        {_, Len} ->
-            genlib:to_int(Len);
-        false ->
-            undefined
-    end.
+log_access(Code, Headers, Body, Req) ->
+    LogFun = cowboy_access_log:get_response_hook(capi_access_lager_event),
+    LogFun(Code, Headers, Body, Req).
--- apps/capi/src/capi_utils.erl
+++ apps/capi/src/capi_utils.erl
@@ -4,6 +4,10 @@
 -export([base64url_to_map/1]).
 -export([map_to_base64url/1]).
 
+-export([to_universal_time/1]).
+
+-export([redact/2]).
+
 -spec logtag_process(atom(), any()) -> ok.
 
 logtag_process(Key, Value) when is_atom(Key) ->
@@ -27,3 +31,57 @@ map_to_base64url(Map) when is_map(Map) ->
             _ = lager:debug("encoding map ~p to base64 failed with ~p:~p", [Map, Class, Reason]),
             erlang:error(badarg)
     end.
+
+-spec redact(Subject :: binary(), Pattern :: binary()) -> Redacted :: binary().
+redact(Subject, Pattern) ->
+    case re:run(Subject, Pattern, [global, {capture, all_but_first, index}]) of
+        {match, Captures} ->
+            lists:foldl(fun redact_match/2, Subject, Captures);
+        nomatch ->
+            Subject
+    end.
+
+redact_match({S, Len}, Subject) ->
+    <<Pre:S/binary, _:Len/binary, Rest/binary>> = Subject,
+    <<Pre/binary, (binary:copy(<<"*">>, Len))/binary, Rest/binary>>;
+redact_match([Capture], Message) ->
+    redact_match(Capture, Message).
+
+-spec to_universal_time(Timestamp :: binary()) -> TimestampUTC :: binary().
+to_universal_time(Timestamp) ->
+    {ok, {Date, Time, Usec, TZOffset}} = rfc3339:parse(Timestamp),
+    Seconds = calendar:datetime_to_gregorian_seconds({Date, Time}),
+    %% The following crappy code is a dialyzer workaround
+    %% for the wrong rfc3339:parse/1 spec.
+    {DateUTC, TimeUTC} = calendar:gregorian_seconds_to_datetime(
+        case TZOffset of
+            _ when is_integer(TZOffset) ->
+                Seconds - (60 * TZOffset);
+            _ ->
+                Seconds
+        end
+    ),
+    {ok, TimestampUTC} = rfc3339:format({DateUTC, TimeUTC, Usec, 0}),
+    TimestampUTC.
+
+%%
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+
+-spec test() -> _.
+
+-spec to_universal_time_test() -> _.
+to_universal_time_test() ->
+    ?assertEqual(<<"2017-04-19T13:56:07Z">>,        to_universal_time(<<"2017-04-19T13:56:07Z">>)),
+    ?assertEqual(<<"2017-04-19T13:56:07.530000Z">>, to_universal_time(<<"2017-04-19T13:56:07.53Z">>)),
+    ?assertEqual(<<"2017-04-19T10:36:07.530000Z">>, to_universal_time(<<"2017-04-19T13:56:07.53+03:20">>)),
+    ?assertEqual(<<"2017-04-19T17:16:07.530000Z">>, to_universal_time(<<"2017-04-19T13:56:07.53-03:20">>)).
+
+-spec redact_test() -> _.
+redact_test() ->
+    P1 = <<"^\\+\\d(\\d{1,10}?)\\d{2,4}$">>,
+    ?assertEqual(<<"+7******3210">>, redact(<<"+79876543210">>, P1)),
+    ?assertEqual(       <<"+1*11">>, redact(<<"+1111">>, P1)).
+
+-endif.
