0deafdf20ae17706980872276d608077eae18b2d
Evgeny Levenets <levenets.e.e@gmail.com>
2018-02-27 14:15:55 +0300

    switch to master damsel & swag

--- apps/capi/src/capi_real_handler.erl
+++ apps/capi/src/capi_real_handler.erl
@@ -2707,11 +2707,11 @@ decode_payment_status({Status, StatusInfo}) ->
 
 decode_operation_failure({operation_timeout, _}) ->
     logic_error(timeout, <<"timeout">>);
-decode_operation_failure({external_failure, #domain_ExternalFailure{
+decode_operation_failure({failure, #domain_Failure{
     code = Code,
-    description = Description
+    reason = Reason
 }}) ->
-    logic_error(Code, Description).
+    logic_error(Code, Reason).
 
 decode_stat_payment(#merchstat_StatPayment{
     id = PaymentID,
@@ -2853,9 +2853,9 @@ merchstat_to_domain({Status, #merchstat_InvoicePaymentFailed{
     }}
 }}) ->
     {Status, #domain_InvoicePaymentFailed{
-        failure = {external_failure, #domain_ExternalFailure{
+        failure = {failure, #domain_Failure{
             code = Code,
-            description = Description
+            reason = Description
         }}
     }};
 merchstat_to_domain({Status, #merchstat_InvoicePaymentFailed{
