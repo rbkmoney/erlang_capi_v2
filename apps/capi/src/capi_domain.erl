a0212a7cb13fa6a8a0a461cdfac528df6ee055b1
Andrey Mayorov <a.mayorov@rbkmoney.com>
2018-05-17 12:54:12 +0300

    Employ dmt_client here

--- apps/capi/src/capi_domain.erl
+++ apps/capi/src/capi_domain.erl
@@ -16,11 +16,7 @@
 
 -spec get_categories(context()) -> {ok, [category()]}.
 get_categories(Context) ->
-    {ok, Snapshot} = get_shapshot(Context),
-    #'Snapshot'{
-        domain = Domain
-    } = Snapshot,
-
+    #'Snapshot'{domain = Domain} = get_shapshot(Context),
     Categories = maps:fold(
         fun
             ({'category', _}, {'category', CategoryObject}, Acc) ->
@@ -38,20 +34,18 @@ get_categories(Context) ->
 get_payment_institutions(Context) ->
     % All this mess was done to reduce requests to dominant.
     % TODO rewrite this with dmt_client, cache, unicorns and rainbows.
-    {ok, Snapshot} = get_shapshot(Context),
-    #'Snapshot'{
-        domain = Domain
-    } = Snapshot,
-    {globals, #domain_GlobalsObject{data = Globals}} = genlib_map:get({globals, #domain_GlobalsRef{}}, Domain),
+    #'Snapshot'{domain = Domain} = get_shapshot(Context),
+    Ref = {globals, #domain_GlobalsRef{}},
+    {ok, {globals, #domain_GlobalsObject{data = Globals}}} = dmt_domain:get_object(Ref, Domain),
     {ok, get_payment_institutions(Globals, Domain)}.
 
 get_payment_institutions(#domain_Globals{payment_institutions = PaymentInstitutionRefs}, Domain)
     when PaymentInstitutionRefs /= undefined
 ->
     lists:map(
-        fun(Ref) ->
-            {_, Obj} = genlib_map:get({payment_institution, Ref}, Domain),
-            Obj
+        fun (Ref) ->
+            {ok, {payment_institution, Object}} = dmt_domain:get_object({payment_institution, Ref}, Domain),
+            Object
         end,
         ordsets:to_list(PaymentInstitutionRefs)
     );
@@ -61,22 +55,19 @@ get_payment_institutions(#domain_Globals{payment_institutions = undefined}, _) -
 -spec get(ref(), context()) -> {ok, data()} | {error, not_found}.
 
 get(Ref, Context) ->
-    {ok, Snapshot} = get_shapshot(Context),
-    #'Snapshot'{
-        domain = Domain
-    } = Snapshot,
-    case genlib_map:get(Ref, Domain) of
-        {_, C} ->
+    #'Snapshot'{domain = Domain} = get_shapshot(Context),
+    case dmt_domain:get_object(Ref, Domain) of
+        {ok, {_Type, C}} ->
             {ok, C};
         undefined ->
             {error, not_found}
     end.
 
-head() ->
-    {'head', #'Head'{}}.
-
 get_shapshot(Context) ->
     get_shapshot(head(), Context).
 
-get_shapshot(Reference, Context) ->
-    capi_woody_client:call_service(repository, 'Checkout', [Reference], Context).
+get_shapshot(Reference, _Context) ->
+    dmt_client:checkout(Reference).
+
+head() ->
+    {'head', #'Head'{}}.
