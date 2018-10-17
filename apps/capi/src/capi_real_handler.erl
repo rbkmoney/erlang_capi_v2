922691ad3cdbb25c55da5de7bb949884390f7cdc
Alexey <kehitt@users.noreply.github.com>
2018-10-17 18:01:47 +0300

    CAPI-307: Fix handling of invalid Residence codes (#279)

--- apps/capi/src/capi_real_handler.erl
+++ apps/capi/src/capi_real_handler.erl
@@ -957,22 +957,27 @@ process_request('GetScheduleByRef', Req, Context) ->
     end;
 
 process_request('GetPaymentInstitutions', Req, #{woody_context := WoodyContext}) ->
-    Residence = encode_residence(genlib_map:get(residence, Req)),
-    Realm = genlib_map:get(realm, Req),
-    {ok, PaymentInstObjects} = capi_domain:get_payment_institutions(WoodyContext),
-    Resp =
-        lists:filtermap(
-            fun(P) ->
-                case check_payment_institution(Realm, Residence, P) of
-                    true ->
-                        {true, decode_payment_institution_obj(P)};
-                    false ->
-                        false
-                end
-            end,
-            PaymentInstObjects
-        ),
-    {ok, {200, [], Resp}};
+    try
+        Residence = encode_residence(genlib_map:get(residence, Req)),
+        Realm = genlib_map:get(realm, Req),
+        {ok, PaymentInstObjects} = capi_domain:get_payment_institutions(WoodyContext),
+        Resp =
+            lists:filtermap(
+                fun(P) ->
+                    case check_payment_institution(Realm, Residence, P) of
+                        true ->
+                            {true, decode_payment_institution_obj(P)};
+                        false ->
+                            false
+                    end
+                end,
+                PaymentInstObjects
+            ),
+        {ok, {200, [], Resp}}
+    catch
+        throw:{encode_residence, invalid_residence} ->
+            {ok, {400, [], logic_error(invalidRequest, <<"Invalid residence">>)}}
+    end;
 
 process_request('GetPaymentInstitutionByRef', Req, #{woody_context := WoodyContext}) ->
     PaymentInstitutionID = genlib:to_int(maps:get(paymentInstitutionID, Req)),
@@ -1064,7 +1069,9 @@ process_request('CreateClaim', Req, Context) ->
         end
     catch
         throw:{encode_contract_modification, adjustment_creation_not_supported} ->
-            {ok, {400, [], logic_error(invalidChangeset, <<"Contract adjustment creation not supported">>)}}
+            {ok, {400, [], logic_error(invalidChangeset, <<"Contract adjustment creation not supported">>)}};
+        throw:{encode_residence, invalid_residence} ->
+            {ok, {400, [], logic_error(invalidRequest, <<"Invalid residence">>)}}
     end;
 
 % TODO disabled temporary, exception handling must be fixed befor enabling
@@ -2023,7 +2030,12 @@ encode_payment_institution_ref(Ref) ->
 encode_residence(undefined) ->
     undefined;
 encode_residence(Residence) when is_binary(Residence) ->
-    list_to_existing_atom(string:to_lower(binary_to_list(Residence))).
+    try
+        list_to_existing_atom(string:to_lower(binary_to_list(Residence)))
+    catch
+        error:badarg ->
+            throw({encode_residence, invalid_residence})
+    end.
 
 decode_residence(undefined) ->
     undefined;
@@ -4238,7 +4250,9 @@ expand_card_info(BankCard, {BinData, Version}) ->
             }
         }
     catch
-        error:badarg ->
+        throw:{encode_binbase_payment_system, invalid_payment_system} ->
+            throw({ok, {400, [], logic_error(invalidRequest, <<"Unsupported card">>)}});
+        throw:{encode_residence, invalid_residence} ->
             throw({ok, {400, [], logic_error(invalidRequest, <<"Unsupported card">>)}})
     end.
 
@@ -4255,7 +4269,8 @@ encode_binbase_payment_system(<<"DISCOVER">>)                  -> discover;
 encode_binbase_payment_system(<<"UNIONPAY">>)                  -> unionpay;
 encode_binbase_payment_system(<<"JCB">>)                       -> jcb;
 encode_binbase_payment_system(<<"NSPK MIR">>)                  -> nspkmir;
-encode_binbase_payment_system(_)                               -> error(badarg).
+encode_binbase_payment_system(_) ->
+    throw({encode_binbase_payment_system, invalid_payment_system}).
 
 enrich_client_info(ClientInfo, Context) ->
     ClientInfo#{<<"ip">> => prepare_client_ip(Context)}.
