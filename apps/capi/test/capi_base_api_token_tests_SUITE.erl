2b00e5521abaa8412c35599f09e5c35cd3aae45e
Roman Pushkov <pushkovroman@me.com>
2019-06-19 17:56:55 +0300

    added more explicit comments in tests

--- apps/capi/test/capi_base_api_token_tests_SUITE.erl
+++ apps/capi/test/capi_base_api_token_tests_SUITE.erl
@@ -1014,7 +1014,13 @@ search_invoices_ok_test(Config) ->
         {invoiceID, <<"testInvoiceID">>},
         {paymentID, <<"testPaymentID">>},
         {payerFingerprint, <<"blablablalbalbal">>},
-        % {lastDigits, <<"2222">>}, %%@FIXME cannot be used until getting the newest api client
+        % %%@FIXME cannot be used until getting the newest api client
+        % swag generates an invalid regex
+        % {pattern, "/^\\d{6,8}$/"} instead of
+        % {pattern, "^\\d{6,8}$"} for the api
+        % the handler is fine
+        %
+        % {lastDigits, <<"2222">>},
         % {bin, <<"424242">>},
         {bankCardTokenProvider, <<"applepay">>},
         {bankCardPaymentSystem, <<"visa">>},
@@ -1040,7 +1046,13 @@ search_payments_ok_test(Config) ->
         {invoiceID, <<"testInvoiceID">>},
         {paymentID, <<"testPaymentID">>},
         {payerFingerprint, <<"blablablalbalbal">>},
-        % {lastDigits, <<"2222">>}, %%@FIXME cannot be used until getting the newest api client
+        % %%@FIXME cannot be used until getting the newest api client
+        % swag generates an invalid regex
+        % {pattern, "/^\\d{6,8}$/"} instead of
+        % {pattern, "^\\d{6,8}$"} for the api
+        % the handler is fine
+        %
+        % {lastDigits, <<"2222">>}
         % {bin, <<"424242">>},
         % {rrn, <<"090909090909">>},
         {bankCardTokenProvider, <<"applepay">>},
