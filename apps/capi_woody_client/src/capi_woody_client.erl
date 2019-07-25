72e77007ddda48ccfa84804c7f70070ad0a862d3
Andrey Fadeev <me@ciiol.net>
2019-07-25 15:33:30 +0300

    Add transport options to services settings

--- apps/capi_woody_client/src/capi_woody_client.erl
+++ apps/capi_woody_client/src/capi_woody_client.erl
@@ -1,9 +1,14 @@
 -module(capi_woody_client).
 
 -export([call_service/4]).
--export([call_service/5]).
 
 -export([get_service_modname/1]).
+
+-type client_opts() :: #{
+    url            := woody:url(),
+    %% See hackney:request/5 for available transport options.
+    transport_opts => woody_client_thrift_http_transport:transport_options()
+}.
 
 %%
 
@@ -12,26 +17,21 @@
 -spec call_service(service_name(), woody:func(), [term()], woody_context:ctx()) ->
     woody:result().
 
-call_service(ServiceName, Function, Args, Context) ->
-    call_service(ServiceName, Function, Args, Context, capi_woody_event_handler).
-
--spec call_service(service_name(), woody:func(), [term()], woody_context:ctx(), woody:ev_handler()) ->
-    woody:result().
-
-call_service(ServiceName, Function, Args, Context0, EventHandler) ->
+call_service(ServiceName, Function, Args, Context0) ->
     Deadline = get_service_deadline(ServiceName),
     Context1 = set_deadline(Deadline, Context0),
     Retry = get_service_retry(ServiceName, Function),
+    EventHandler = capi_woody_event_handler,
     call_service(ServiceName, Function, Args, Context1, EventHandler, Retry).
 
 call_service(ServiceName, Function, Args, Context, EventHandler, Retry) ->
-    Url = get_service_url(ServiceName),
+    Options = get_service_options(ServiceName),
     Service = get_service_modname(ServiceName),
     Request = {Service, Function, Args},
     try
         woody_client:call(
             Request,
-            #{url => Url, event_handler => EventHandler},
+            Options#{event_handler => EventHandler},
             Context
         )
     catch
@@ -41,6 +41,17 @@ call_service(ServiceName, Function, Args, Context, EventHandler, Retry) ->
             NextRetry = apply_retry_strategy(Retry, Error, Context),
             call_service(ServiceName, Function, Args, Context, EventHandler, NextRetry)
     end.
+
+-spec get_service_options(atom()) ->
+    client_opts().
+
+get_service_options(ServiceName) ->
+    construct_opts(maps:get(ServiceName, genlib_app:env(?MODULE, services))).
+
+construct_opts(Opts = #{url := Url}) ->
+    Opts#{url := genlib:to_binary(Url)};
+construct_opts(Url) ->
+    #{url => genlib:to_binary(Url)}.
 
 apply_retry_strategy(Retry, Error, Context) ->
     apply_retry_step(genlib_retry:next_step(Retry), woody_context:get_deadline(Context), Error).
@@ -62,9 +73,6 @@ apply_retry_step({wait, Timeout, Retry}, Deadline0, Error) ->
             ok = timer:sleep(Timeout),
             Retry
     end.
-
-get_service_url(ServiceName) ->
-    maps:get(ServiceName, genlib_app:env(?MODULE, service_urls)).
 
 -spec get_service_modname(service_name()) -> woody:service().
 
