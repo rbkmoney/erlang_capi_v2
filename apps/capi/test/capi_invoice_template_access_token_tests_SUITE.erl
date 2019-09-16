15745e6aa96057dc357327ec1af24663c7f253f9
Sergey Yelin <elinsn@gmail.com>
2019-09-16 20:18:13 +0300

    Upgrade damsel (#422)

--- apps/capi/test/capi_invoice_template_access_token_tests_SUITE.erl
+++ apps/capi/test/capi_invoice_template_access_token_tests_SUITE.erl
@@ -2,8 +2,8 @@
 
 -include_lib("common_test/include/ct.hrl").
 
--include_lib("dmsl/include/dmsl_domain_config_thrift.hrl").
--include_lib("dmsl/include/dmsl_payment_processing_thrift.hrl").
+-include_lib("damsel/include/dmsl_domain_config_thrift.hrl").
+-include_lib("damsel/include/dmsl_payment_processing_thrift.hrl").
 -include_lib("capi_dummy_data.hrl").
 -include_lib("jose/include/jose_jwk.hrl").
 
