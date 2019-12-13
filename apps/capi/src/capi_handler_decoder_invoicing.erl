573f6df303afd811101c9d8279c736075d3ac417
Andrey Fadeev <me@ciiol.net>
2019-12-13 13:42:19 +0300

    Fix CryptoWallet payment method decoding

--- apps/capi/src/capi_handler_decoder_invoicing.erl
+++ apps/capi/src/capi_handler_decoder_invoicing.erl
@@ -420,12 +420,13 @@ decode_payment_method(digital_wallet, Providers) ->
     [#{<<"method">> => <<"DigitalWallet">>, <<"providers">> => lists:map(fun genlib:to_binary/1, Providers)}];
 decode_payment_method(tokenized_bank_card, TokenizedBankCards) ->
     decode_tokenized_bank_cards(TokenizedBankCards);
-decode_payment_method(crypto_wallet, CryptoCurrencies) ->
-    [#{
-        <<"method">> => <<"CryptoWallet">>,
-        <<"cryptoCurrency">> =>
-            lists:map(fun capi_handler_decoder_utils:convert_crypto_currency_to_swag/1, CryptoCurrencies)
-    }];
+decode_payment_method(crypto_currency, CryptoCurrencies) ->
+    [
+        #{
+            <<"method">> => <<"CryptoWallet">>,
+            <<"cryptoCurrency">> => capi_handler_decoder_utils:convert_crypto_currency_to_swag(C)
+        } || C <- CryptoCurrencies
+    ];
 decode_payment_method(mobile, MobileOperators) ->
     [#{<<"method">> => <<"MobileCommerce">>, <<"operators">> => lists:map(fun genlib:to_binary/1, MobileOperators)}].
 
