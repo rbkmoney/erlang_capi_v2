831ddbfb5645724dd6e8f970626667d433015684
Sergey Yelin <elinsn@gmail.com>
2020-03-03 19:08:55 +0300

    Remove warning

--- apps/capi/src/capi_swagger_server.erl
+++ apps/capi/src/capi_swagger_server.erl
@@ -87,6 +87,5 @@ mk_operation_id_getter(#{env := Env}) ->
                 #{}
         end;
         (_Req) ->
-            logger:warning("Partial request detected (no host or path in request), skip getting operation_id"),
             #{}
     end.
