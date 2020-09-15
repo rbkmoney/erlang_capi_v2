98dce1bf2cc346dbeda4cc7c7449e9f9e2799711
0x42 <bpozdnyakov@gmail.com>
2020-09-15 13:24:25 +0300

    typo

--- apps/capi/include/capi_feature_schemas.hrl
+++ apps/capi/include/capi_feature_schemas.hrl
@@ -1,8 +1,8 @@
 -ifndef(__capi_feature_schemas__).
 -define(__capi_feature_schemas__, 42).
 
-% Marking some feature as `descriminator` will make featureset comparator consider two sets with different
-% `descriminator` values as _different everywhere_ which usually helps with diff readability.
+% Marking some feature as `discriminator` will make featureset comparator consider two sets with different
+% `discriminator` values as _different everywhere_ which usually helps with diff readability.
 -define(discriminator,    1).
 -define(difference,      -1).
 
