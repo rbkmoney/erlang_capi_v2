673c38e46e06ad166d96cd7fd3725996bcde8337
dinama <dinama@users.noreply.github.com>
2021-03-09 18:03:01 +0300

    ED-55: +geo bouncer +parties bouncer +customer bouncer

--- apps/capi/src/capi_handler_geo.erl
+++ apps/capi/src/capi_handler_geo.erl
@@ -13,34 +13,32 @@
     Req :: capi_handler:request_data(),
     Context :: capi_handler:processing_context()
 ) -> {ok, capi_handler:request_state()} | {error, noimpl}.
-prepare(OperationID, Req, Context) when OperationID =:= 'GetLocationsNames' ->
-    Authorize = fun() -> {ok, capi_auth:authorize_operation(OperationID, [], Context, Req)} end,
-    Process = fun() -> process_request(OperationID, Context, Req) end,
-    {ok, #{authorize => Authorize, process => Process}};
-prepare(_OperationID, _Req, _Context) ->
-    {error, noimpl}.
 
--spec process_request(
-    OperationID :: capi_handler:operation_id(),
-    Context :: capi_handler:processing_context(),
-    ReqState :: capi_handler:request_state()
-) -> {ok, capi_handler:response()}.
-process_request('GetLocationsNames', Context, Req) ->
-    CallArgs = {ordsets:from_list(maps:get('geoIDs', Req)), maps:get('language', Req)},
-    Call = {geo_ip_service, 'GetLocationName', CallArgs},
-    case capi_handler_utils:service_call(Call, Context) of
-        {ok, LocationNames = #{}} ->
-            PreparedLocationNames =
-                maps:fold(
+prepare('GetLocationsNames' = OperationID, Req, Context) ->
+    Authorize = fun() ->
+        Prototypes = [{operation, #{id => OperationID}}],
+        {ok, capi_auth:authorize_operation(OperationID, Prototypes, Context, Req)}
+    end,
+    Process = fun() ->
+        GeoIDs = ordsets:from_list(maps:get('geoIDs', Req)),
+        Language = maps:get('language', Req),
+        Call = {geo_ip_service, 'GetLocationName', {GeoIDs, Language}},
+        case capi_handler_utils:service_call(Call, Context) of
+            {ok, LocationNames} ->
+                PreparedLocationNames = maps:fold(
                     fun(GeoID, Name, Acc) -> [decode_location_name(GeoID, Name) | Acc] end,
                     [],
                     LocationNames
                 ),
-            {ok, {200, #{}, PreparedLocationNames}};
-        {exception, #'InvalidRequest'{errors = Errors}} ->
-            FormattedErrors = capi_handler_utils:format_request_errors(Errors),
-            {ok, logic_error(invalidRequest, FormattedErrors)}
-    end.
+                {ok, {200, #{}, PreparedLocationNames}};
+            {exception, #'InvalidRequest'{errors = Errors}} ->
+                FormattedErrors = capi_handler_utils:format_request_errors(Errors),
+                {ok, logic_error(invalidRequest, FormattedErrors)}
+        end
+    end,
+    {ok, #{authorize => Authorize, process => Process}};
+prepare(_OperationID, _Req, _Context) ->
+    {error, noimpl}.
 
 %%
 
