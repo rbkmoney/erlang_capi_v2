5c0fe9c2959413730f3226588797ea6068e99369
Yaroslav Rogov <rogovyaroslav@gmail.com>
2021-07-06 14:51:33 +0300

    ED-190/deps: Update dmt_client

--- apps/capi/src/capi_handler_trade_blocs.erl
+++ apps/capi/src/capi_handler_trade_blocs.erl
@@ -24,7 +24,7 @@ prepare(_OperationID, _Req, _Context) ->
 
 process_request('GetTradeBlocs', _Req, #{woody_context := WoodyContext}) ->
     TradeBlocs = unwrap(capi_domain:get_objects_by_type(trade_bloc, WoodyContext)),
-    {ok, {200, #{}, lists:map(fun decode_trade_bloc_object/1, TradeBlocs)}};
+    {ok, {200, #{}, lists:map(fun decodbe_trade_bloc_object/1, TradeBlocs)}};
 process_request('GetTradeBlocByID', Req, #{woody_context := WoodyContext}) ->
     Ref = {trade_bloc, #domain_TradeBlocRef{id = maps:get('tradeBlocID', Req)}},
     case capi_domain:get(Ref, WoodyContext) of
